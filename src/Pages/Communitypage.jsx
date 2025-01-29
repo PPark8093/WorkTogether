5d9d401c6d68dd9196c75528bf251217c88c2262
PPark8093 <parkjuwon8093@outlook.kr>
2025-01-29 23:08:15 +0900

    fourth

--- src/Pages/Communitypage.jsx
+++ src/Pages/Communitypage.jsx
@@ -9,23 +9,36 @@ function Communitypage() {
     const [body, setBody] = useState("");
 
     useEffect(() => {
-        axios.get("https://worktogetherserver.vercel.app/posts").then((res) => {setPosts(res.data);}).catch((error) => console.error("Error", error));
+        get_posts();
     }, []);
 
     const get_posts = () => {
         axios.get("https://worktogetherserver.vercel.app/posts").then((res) => {setPosts(res.data);}).catch((error) => console.error("Error", error));
     }
 
-    const submit_post = () => {
+    // 새로운 게시글 추가
+    const add_post = () => {
         if (!title || !body) {
             alert("title이나 body칸을 입력하세요!");
             return;
         } else {
-            const newPost = { title, body };
+            const newPost = { title, body, like: 0 };
             axios.post("https://worktogetherserver.vercel.app/posts", newPost).then((res) => {setPosts([res.data, ...posts]); setTitle(""); setBody("");}).catch((error) => console.error("Error", error));    
         }
 
         get_posts();
+    };
+
+    // 이미 있는 게시글에 좋아요만 추가
+    const increase_like = (post) => {
+        const updatedPost = { ...post, like: post.like + 1 };
+
+        axios
+            .post("https://worktogetherserver.vercel.app/posts", updatedPost)
+            .then((res) => {
+                setPosts(posts.map(p => p._id === post._id ? res.data : p));
+            })
+            .catch((error) => console.error("Error", error));
     };
 
     return (
@@ -34,15 +47,15 @@ function Communitypage() {
             <div className='add_container'>
                 <input placeholder='제목' value={title} onChange={(event) => setTitle(event.target.value)} required/>
                 <input placeholder='내용' value={body} onChange={(event) => setBody(event.target.value)} required/>
-                <button onClick={submit_post}>추가하기</button>
+                <button onClick={add_post}>추가하기</button>
             </div>
             
-            {Array.isArray(posts) && posts.length > 0 ? <div className='post_list_container'>
+            {Array.isArray(posts) && posts.length > 0 ? (<div className='post_list_container'>
                 {posts.map((post, index) => (
                     <div key={post.id || index} className='post_container'>
                         <div style={{display: "flex", alignItems: "center", justifyContent: "center", flexDirection: "column"}}>
-                            <button className='heart_button'>좋</button>
-                            <p>0</p>
+                            <button className='heart_button' onClick={() => increase_like(post)}>좋</button>
+                            <p>{post.like}</p>
                         </div>
                         <div style={{marginLeft: "1rem"}}>
                             <h2>{post.title}</h2>
@@ -50,7 +63,7 @@ function Communitypage() {
                         </div>
                     </div>
                 ))}
-            </div> : <div className='no_posts_container'><h1>현재 게시글이 없습니다!</h1></div>}
+            </div>) : <div className='no_posts_container'><h1>현재 게시글이 없습니다!</h1></div>}
 
             <br />
             <a>(좋아요 기능 아직 덜만듬)</a>
