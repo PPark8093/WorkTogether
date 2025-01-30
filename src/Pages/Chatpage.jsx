e3284679cde0710533e82fd06852785b9537ee04
PPark8093 <parkjuwon8093@outlook.kr>
2025-01-30 15:10:15 +0900

    sixth

--- src/Pages/Chatpage.jsx
+++ src/Pages/Chatpage.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import TitleBar from '../Utils/TitleBar'
+import "../Styles/Chatpage.css"
 
 function Chatpage() {
+
     return (
         <div>
-            <TitleBar title={"채팅"} goto={"/"}/>
-            <p>(만드는 중)</p>
+            <TitleBar title={"채팅"} goto={"/"} />
+            <div className='chat_container'>
+                <textarea rows={1} style={{height: "100%"}} />
+                <div className='input_container'>
+                    <input style={{flex: 1}} placeholder='ID'/>
+                    <input style={{flex: 9}} placeholder='입력' />
+                </div>
+                <button>전송하기</button>
+            </div>
         </div>
     )
 }
